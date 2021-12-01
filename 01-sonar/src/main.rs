53669501479ddc2d847e1e9a925a0576dc72ac1f
Riley Shahar <riley.shahar@gmail.com>
2021-11-30 23:25:26 -0800

    Merge methods to one file
    This is just cleaner

--- 01-sonar/src/main.rs
+++ 01-sonar/src/main.rs
@@ -1,29 +1,16 @@
 use itertools::Itertools;
 
-/// Preprocess the input string into an iterator of usizes.
-fn preprocess_input(input: &'static str) -> impl Iterator<Item = usize> {
+/// Solve the problem.
+fn solve(input: &'static str) -> usize {
     input
         .trim_end() // remove trailing `\n`
         .split('\n')
-        .map(str::parse)
+        .map(str::parse::<usize>)
         .map(|r| r.expect("input is valid usizes"))
-}
-
-/// Group depths by sum of rolling windows of three.
-fn group_by_three(depths: impl Iterator<Item = usize>) -> impl Iterator<Item = usize> {
-    depths.tuple_windows().map(|(p, c, n)| p + c + n)
-}
-
-/// Compute the number of times the sequence increases.
-fn compute_increases(depths: impl Iterator<Item = usize>) -> usize {
-    depths
+        .tuple_windows()
+        .map(|(p, c, n)| p + c + n)
         .tuple_windows()
         .fold(0, |t, (p, c)| if c > p { t + 1 } else { t })
-}
-
-/// Solve the problem.
-fn solve(input: &'static str) -> usize {
-    compute_increases(group_by_three(preprocess_input(input)))
 }
 
 fn main() {
