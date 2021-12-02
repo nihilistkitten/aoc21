51cdac407ecd0cbbdad2bd21a51ec97352c55a15
Riley Shahar <riley.shahar@gmail.com>
2021-12-01 21:55:59 -0800

    Impl problem 2 part 1

---
+++ 02-dive/src/main.rs
@@ -0,0 +1,81 @@
+use std::str::FromStr;
+
+use utils::aoc_problem;
+
+#[derive(Debug, thiserror::Error, PartialEq)]
+enum Error {
+    #[error("invalid str: {0}")]
+    StepParse(String),
+
+    #[error("invalid direction: {0}")]
+    InvalidDirection(String),
+
+    #[error("{0}")]
+    NotAnInt(#[from] std::num::ParseIntError),
+}
+
+#[derive(Default)]
+struct Position {
+    depth: u32,
+    horizontal: u32,
+}
+
+impl Position {
+    const fn apply(mut self, step: Step) -> Self {
+        match step {
+            Step::Up(x) => self.depth -= x,
+            Step::Down(x) => self.depth += x,
+            Step::Forward(x) => self.horizontal += x,
+        }
+        self
+    }
+
+    const fn product(&self) -> u32 {
+        self.depth * self.horizontal
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Step {
+    Up(u32),
+    Down(u32),
+    Forward(u32),
+}
+
+impl FromStr for Step {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (direction, size) = s
+            .split_once(' ')
+            .ok_or_else(|| Error::StepParse(s.into()))?;
+        let n = size.parse()?;
+        Ok(match direction {
+            "up" => Self::Up(n),
+            "down" => Self::Down(n),
+            "forward" => Self::Forward(n),
+            _ => return Err(Error::InvalidDirection(direction.into())),
+        })
+    }
+}
+
+fn solve(input: &'static str) -> u32 {
+    input
+        .trim_end() // remove trailing `\n`
+        .split('\n')
+        .map(str::parse::<Step>)
+        .map(|r| r.expect("input is valid steps"))
+        .fold(Position::default(), Position::apply)
+        .product()
+}
+
+aoc_problem!(example_soln = 150);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn step_parser_works() {
+        assert_eq!("up 2".parse(), Ok(Step::Up(2)));
+    }
+}
