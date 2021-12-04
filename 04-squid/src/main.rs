4fdfee74ee0154bb8ef6b4062974a594203f53b3
Riley Shahar <riley.shahar@gmail.com>
2021-12-04 14:42:09 -0800

    Impl problem 4 part 1

---
+++ 04-squid/src/main.rs
@@ -0,0 +1,106 @@
+use itertools::Itertools;
+
+use utils::aoc_problem;
+
+type Row = [u32; 5];
+
+#[derive(Debug)]
+struct Board([Row; 5]);
+
+fn take_five<T>(iter: &mut impl Iterator<Item = T>) -> Option<[T; 5]> {
+    Some([
+        iter.next()?,
+        iter.next()?,
+        iter.next()?,
+        iter.next()?,
+        iter.next()?,
+    ])
+}
+
+impl Board {
+    fn from_iterator(mut iter: impl Iterator<Item = u32>) -> Option<Self> {
+        Some(Self([
+            take_five(&mut iter)?,
+            take_five(&mut iter)?,
+            take_five(&mut iter)?,
+            take_five(&mut iter)?,
+            take_five(&mut iter)?,
+        ]))
+    }
+
+    fn sum_unmarked(&self, marked: &[u32]) -> u32 {
+        self.0
+            .iter()
+            .flatten()
+            .filter(|x| !marked.contains(x))
+            .sum()
+    }
+
+    /// Get the score.
+    ///
+    /// # Panics
+    /// Panics if `marked` is empty.
+    fn score(&self, marked: &[u32]) -> u32 {
+        self.sum_unmarked(marked) * marked.last().expect("at least one marked square for bingo")
+    }
+
+    /// Check if the board has bingo, returning the score.
+    fn bingod(&self, marked: &[u32]) -> Option<u32> {
+        for i in 0..5 {
+            // check columns
+            if (0..5).all(|j| marked.contains(&self.0[i][j])) {
+                return Some(self.score(marked));
+            }
+
+            // check rows
+            if (0..5).all(|j| marked.contains(&self.0[j][i])) {
+                return Some(self.score(marked));
+            }
+        }
+
+        None
+    }
+}
+
+/// Solve the problem.
+fn solve(input: &'static str) -> u32 {
+    let mut inputs = input
+        .trim_end() // remove trailing `\n`
+        .split('\n');
+
+    let lottery_numbers = inputs
+        .next()
+        .unwrap()
+        .split(',')
+        .map(str::parse::<u32>)
+        .map(|r| r.expect("lottery numbers are valid u32s"));
+
+    let chunks = inputs.chunks(6);
+
+    let boards: Vec<_> = chunks
+        .into_iter()
+        .map(|chunk| {
+            Board::from_iterator(
+                chunk
+                    .skip(1) // skip separating line
+                    .flat_map(str::split_whitespace)
+                    .map(str::parse::<u32>)
+                    .map(|r| r.expect("input is valid u32s")),
+            )
+            .expect("input is well-formed")
+        })
+        .collect();
+
+    let mut called = vec![];
+
+    for number in lottery_numbers {
+        called.push(number);
+        if let Some(score) = boards.iter().find_map(|b| b.bingod(&called)) {
+            return score;
+        }
+    }
+
+    unreachable!("the problem guarantees some buard eventually wins")
+}
+
+aoc_problem!(example_soln = 4512);
