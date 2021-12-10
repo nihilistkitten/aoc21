606b9575bfd6a8a2b6c57f0ecda20a8f29a46ba3
Riley Shahar <riley.shahar@gmail.com>
2021-12-09 23:35:57 -0800

    Impl problem 9 part 2

--- 09-smoke-basin/src/main.rs
+++ 09-smoke-basin/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 use utils::aoc_problem;
@@ -5,29 +7,48 @@ use utils::aoc_problem;
 type Grid = Vec<Vec<u32>>;
 
 /// Get values of adjacent elements. Diagonals are not adjacent.
-fn neighbors(g: &Grid, x: usize, y: usize) -> Vec<u32> {
+fn neighbors(g: &Grid, x: usize, y: usize) -> Vec<(usize, usize)> {
     let mut out = vec![];
     if x > 0 {
-        out.push(g[x - 1][y]);
+        out.push((x - 1, y));
     }
     if x < g.len() - 1 {
-        out.push(g[x + 1][y]);
+        out.push((x + 1, y));
     }
     if y > 0 {
-        out.push(g[x][y - 1]);
+        out.push((x, y - 1));
     }
     if y < g[x].len() - 1 {
-        out.push(g[x][y + 1]);
+        out.push((x, y + 1));
+    }
+    out
+}
+
+/// Simple flood fill with a vector as a stack.
+fn flood_fill(g: &Grid, x: usize, y: usize) -> HashSet<(usize, usize)> {
+    let mut stack = vec![(x, y)];
+    let mut out = HashSet::default();
+    while !stack.is_empty() {
+        let curr = stack.pop().unwrap();
+        if g[curr.0][curr.1] == 9 {
+            continue;
+        }
+        out.insert(curr);
+        for node in neighbors(g, curr.0, curr.1) {
+            if !out.contains(&node) {
+                stack.push(node);
+            }
+        }
     }
     out
 }
 
 fn is_low_point(g: &Grid, x: usize, y: usize) -> bool {
-    neighbors(g, x, y).iter().all(|&n| n > g[x][y])
+    neighbors(g, x, y).iter().all(|&(p, q)| g[p][q] > g[x][y])
 }
 
 /// Solve the problem.
-fn solve(input: &'static str) -> u32 {
+fn solve(input: &'static str) -> usize {
     let grid: Grid = input
         .trim_end() // remove trailing `\n`
         .split('\n')
@@ -42,8 +63,11 @@ fn solve(input: &'static str) -> u32 {
     (0..grid.len())
         .cartesian_product(0..grid[0].len())
         .filter(|&(x, y)| is_low_point(&grid, x, y))
-        .map(|(x, y)| grid[x][y] + 1)
-        .sum()
+        .map(|(x, y)| flood_fill(&grid, x, y).len())
+        .sorted()
+        .rev()
+        .take(3)
+        .product()
 }
 
-aoc_problem!(example_soln = 15);
+aoc_problem!(example_soln = 1134);
